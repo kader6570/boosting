04f6bc17d806d42c3c26d35b3b524b824867f28b
kader6570 <ka6570der@icloud.com>
2025-12-08 19:52:59 +0000

    nombre par liste

--- boosting/admin.py
+++ boosting/admin.py
@@ -4,5 +4,5 @@ from .models import Inscription
 # Register your models here.
 class AdminInscription(admin.ModelAdmin):
     list_display = ('tiktok_username','numero')
-
+    list_per_page=100
 admin.site.register(Inscription,AdminInscription)
